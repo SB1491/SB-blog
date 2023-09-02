c394f496ebbd7c6dec3d2b5fbbb51aa950d1e822
SB1491 <etaeyoung2@snu.ac.kr>
2023-09-02 15:22:30 +0900

    linklist 구현

--- frontend/src/components/LinkList/LinkList.tsx
+++ frontend/src/components/LinkList/LinkList.tsx
@@ -1,7 +1,24 @@
-const LinkList = () => {
+interface Link {
+  text: string;
+  url: string;
+}
+
+interface LinkListProps {
+  links: Link[];
+}
+
+
+const LinkList: React.FC<LinkListProps> = ({ links }) => {
   return (
-    <></>
+    <ul>
+      {links.map((link, index) => (
+        <li key={index}>
+          <a href={link.url}>{link.text}</a>
+        </li>
+      ))}
+    </ul>
   )
 }
+
 
 export default LinkList
--- frontend/src/containers/Linguistics/Linguistics.tsx
+++ frontend/src/containers/Linguistics/Linguistics.tsx
@@ -1,10 +1,18 @@
 import Header from "../../components/Header/Header"
+import LinkList from "../../components/LinkList/LinkList"
 
 const Linguistics = () => {
+
+  const links = [
+    { text: 'Google', url: 'https://www.google.com' },
+    { text: 'GitHub', url: 'https://www.github.com' },
+  ]
+
   return (
     <>
       <Header/>
       <h1>Linguistics</h1>
+      <LinkList links={links}/>
     </>
   )
 }
